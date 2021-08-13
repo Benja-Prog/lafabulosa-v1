eebcf1c67547c06dbd9186c2b77f70273ed57b7c
Benjamin <rey-colombia@hotmail.com>
2021-08-13 01:50:18 -0500

    Cambio en .env

--- src/config/connectiondb.js
+++ src/config/connectiondb.js
@@ -6,7 +6,7 @@ const mysql = require('mysql');
 const connection = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS,
     database:  process.env.DB_DATABASE
 };
 
