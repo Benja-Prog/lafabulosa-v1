b966803f5ad3be0f96d741aee046140708963f2c
Benjamin <rey-colombia@hotmail.com>
2021-08-13 01:38:48 -0500

    retorno a confi db

--- src/config/connectiondb.js
+++ src/config/connectiondb.js
@@ -4,21 +4,34 @@ const mysql = require('mysql');
 //y ligado a la carpeta ./env/.env
 
 const connection = mysql.createConnection({
-    host: 'us-cdbr-east-04.cleardb.com',
-    password: '44bbe039',
-    user: 'b74959fd67a0b2',
-    database: 'heroku_de58209aa376fbb'
-});
-//mysql://b74959fd67a0b2:44bbe039@us-cdbr-east-04.cleardb.com/hem/heroku_de58209aa376fbb?reconnect=true
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database:  process.env.DB_DATABASE,
+}) 
 
-//probar el modulo:
 
-connection.connect((err) => {
-    if(err){
-        console.log("El error de conexion a BD es: " + err)
-        // return res.redirect('./500.ejs');
-    }
-    console.log("Conectado exitosamente a BD");
-})
+// Probar modulo
+
+function handleDisconnect(connection){
+    connection = mysql.createPool(connection);
+
+    connection.getConnection(function(err){
+        if(err){
+            console.log('error al conectarse a db: ', err);
+            setTimeout(handleDisconnect, 2000);
+        }
+    });
+
+    connection.on('error', function(err){
+        console.log('DB Error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+            handleDisconnect();
+        }else{
+            throw err;
+        }
+    });
+}
+handleDisconnect(connection);
 
 module.exports = connection;
