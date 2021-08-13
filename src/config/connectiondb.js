8341c7f3d46b2da8034d1c1bc5b8c9883635116d
Benjamin <rey-colombia@hotmail.com>
2021-08-13 01:44:52 -0500

    cambio en connection db

--- src/config/connectiondb.js
+++ src/config/connectiondb.js
@@ -3,12 +3,12 @@ const mysql = require('mysql');
 //funciona porque el servidor esta conectado con anterioridad a dotenv que esta configurado
 //y ligado a la carpeta ./env/.env
 
-const connection = mysql.createConnection({
+const connection = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database:  process.env.DB_DATABASE
-}) 
+};
 
 
 // Probar modulo
